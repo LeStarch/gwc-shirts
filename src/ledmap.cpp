56aab9c6487a02981f9b408da75c448c0af00274
M Starch <lestarch@googlemail.com>
2019-03-02 00:26:12 -0800

    orage change 1

--- src/ledmap.cpp
+++ src/ledmap.cpp
@@ -12,9 +12,12 @@
 
 //Order of LEDs
 int OUTPUT_LEDS[PIN_COUNT] = {PIN_2, PIN_12, PIN_7, PIN_9, HEALTH};
+// PIN_2 - left ear
+// PIN_12
+// PIN_7 -- right ear
+// PIN_9 --
 
 //Order of functions (same as above).
 //CHANGE ME: to assign different behavior to the above pins.
-LedFunction* FUNC_LEDS[PIN_COUNT] = {new BlinkFast(), new BlinkBurst(), new BlinkSlow(), new BlinkBurst(), new BlinkSlow()};
-
+LedFunction* FUNC_LEDS[PIN_COUNT] = {new BlinkSlow(), new BlinkFast(), new BlinkSlow(), new BlinkSlow(), new BlinkSlow()};
 
